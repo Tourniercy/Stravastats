cb7ccc2643324d6f45e2f5f126f7c9ac9e75c5cc
Cyril <tourniercy@gmail.com>
2024-09-13 22:27:04 +0200

    feat:add auth handler

---
+++ src/auth.ts
@@ -0,0 +1,43 @@
+import NextAuth from "next-auth"
+import "next-auth/jwt"
+import Strava from "next-auth/providers/strava"
+
+
+declare module "next-auth" {
+    interface Session {
+        accessToken?: string
+    }
+}
+
+declare module "next-auth/jwt" {
+    interface JWT {
+        accessToken?: string
+    }
+}
+
+export const { handlers, auth, signIn } = NextAuth({
+    providers: [
+        Strava({ authorization: { params: { scope: "read_all" } } }),
+    ],
+    callbacks: {
+        authorized({ request, auth }) {
+            const { pathname } = request.nextUrl
+            if (pathname === "/middleware-example") return !!auth
+            return true
+        },
+        jwt({ token, trigger, session, account }) {
+            if (trigger === "update") token.name = session.user.name
+            if (account?.provider === "keycloak") {
+                return { ...token, accessToken: account.access_token }
+            }
+            return token
+        },
+        async session({ session, token }) {
+            if (token?.accessToken) {
+                session.accessToken = token.accessToken
+            }
+            return session
+        },
+    },
+});
+

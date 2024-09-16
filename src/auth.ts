24910bcb8c3ab53b848e3d7b7df3953ab303f46f
Cyril <tourniercy@gmail.com>
2024-09-16 21:56:30 +0200

    feat(deps): add biome to dependencies and fix typescript issues

--- src/auth.ts
+++ src/auth.ts
@@ -1,43 +1,53 @@
-import NextAuth, {Account, User} from "next-auth"
-import "next-auth/jwt"
-import Strava from "next-auth/providers/strava"
+import NextAuth, { type Account, type Profile } from "next-auth";
+import "next-auth/jwt";
+import type { AdapterUser } from "@auth/core/adapters";
+import Strava from "next-auth/providers/strava";
 
 declare module "next-auth" {
-    interface Session {
-        user: any
-        account: any
-        profile: any
-    }
+	interface Session {
+		user: CustomUser;
+		account: Account;
+		profile: Profile;
+	}
 }
 
+type CustomUser = AdapterUser & {
+	emailVerified: Date | null;
+	isEmailVerified: boolean;
+};
 
 export const { handlers, auth, signIn } = NextAuth({
-    providers: [
-        Strava({ authorization: { params: { scope: "read_all" }, responseType: "code" } }),
-    ],
-    callbacks: {
-        async jwt({ token, user, account, profile }) {
-            if (user) {
-                token.user = user
-                token.account = account
-                token.profile = profile
-            }
-            return token
-        },
-        async session({ session, token }) {
-            session.user = token.user
-            session.account = token.account
-            session.profile = token.profile
-            return session
-        },
-        async signIn({ user, account, profile }) {
-            console.log(user, account, profile)
-            return true
-        },
-        authorized: async ({ auth }) => {
-            // Logged in users are authenticated, otherwise redirect to login page
-            return !!auth
-        },
-    },
-
+	providers: [
+		Strava({
+			authorization: { params: { scope: "read_all" }, responseType: "code" },
+		}),
+	],
+	callbacks: {
+		async jwt({ token, user, account, profile }) {
+			if (user) {
+				token.user = user;
+				token.account = account;
+				token.profile = profile;
+			}
+			return token;
+		},
+		async session({ session, token }) {
+			const adapterUser = token.user as AdapterUser;
+			session.user = {
+				...adapterUser,
+				emailVerified: adapterUser.emailVerified,
+				isEmailVerified: adapterUser.emailVerified instanceof Date,
+			};
+			session.account = token.account as Account;
+			session.profile = token.profile as Profile;
+			return session;
+		},
+		async signIn({ user, account, profile }) {
+			console.log(user, account, profile);
+			return true;
+		},
+		authorized: async ({ auth }) => {
+			return !!auth;
+		},
+	},
 });
